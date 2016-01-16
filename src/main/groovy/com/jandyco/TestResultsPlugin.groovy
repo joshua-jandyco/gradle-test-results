4d0d861d54bcff519aa253c4170377b69279f1cf
Josh <joshua-jandyco@users.noreply.github.com>
2016-01-16 13:56:34 -0500

    Initial checkin of skeleton files

---
+++ src/main/groovy/com/jandyco/TestResultsPlugin.groovy
@@ -0,0 +1,8 @@
+package com.jandyco
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class TestResultsPlugin implements Plugin<Project> {
+    void apply(Project project) { }
+}
---
+++ src/test/groovy/com/jandyco/TestResultsPluginTest.groovy
@@ -0,0 +1,5 @@
+package com.jandyco
+
+import spock.lang.*
+
+class TestResultsPluginTest extends Specification { }
